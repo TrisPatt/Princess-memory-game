f7856ae49ecdad0ba97b19423723af1ff4329dc2
TrisPatt <tristanpattison@yahoo.co.uk>
2024-03-01 15:39:55 +0000

    add basic styling in CSS

--- script.js
+++ script.js
@@ -8,17 +8,24 @@ const cardPickArray = [
     {name: 'Moana', img: 'assets/images/Moana.webp'},
     {name: 'Rapunzel', img: 'assets/images/Rapunzel.webp'},
     {name: 'Tiana', img: 'assets/images/Tiana.webp'},
-    
 ]
+
 //To duplicate the cards to make pairs
 const cards = [...cardPickArray, ...cardPickArray];
 
 //Randomise cards
 cards.sort(() => 0.5 - Math.random());
 
-
 //To create the game area
-
-
+const gameArea = document.querySelector('#game-area')
+function createCards() {
+    for (let i = 0; i< cards.length; i++) {
+    let card = document.createElement('img');
+    card.setAttribute('src' , 'assets/images/Disney_Princess_logo_PNG2.png');
+    card.setAttribute('data-Id', i);
+    gameArea.appendChild(card);
+    }
+}
+createCards();
 
 
