3ce0f4e19dd376e006ad89482b487dc6e5395d1d
TrisPatt <tristanpattison@yahoo.co.uk>
2024-03-01 11:28:41 +0000

    add object array of cards in JS

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,18 @@
-console.log("connected!")
+//create an array of objects for the 16 cards
+const cardPickArray = [
+    {name: 'Ariel', img: 'assets/images/Ariel.webp'},
+    {name: 'Aurora', img: 'assets/images/Aurora.webp'},
+    {name: 'Belle', img: 'assets/images/Belle.webp'},
+    {name: 'Cinderella', img: 'assets/images/cinderella.webp'},
+    {name: 'Jasmine', img: 'assets/images/Jasmine.webp'},
+    {name: 'Moana', img: 'assets/images/Moana.webp'},
+    {name: 'Rapunzel', img: 'assets/images/Rapunzel.webp'},
+    {name: 'Tiana', img: 'assets/images/Tiana.webp'},
+    
+]
+
+//To duplicate the cards
+const cards = [...cardPickArray, ...cardPickArray];
+
+console.log(cards);
+
